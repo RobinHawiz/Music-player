9899809c1116b9a51008783f5bc7fb8bf83ec4c2
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-01 22:13:07 +0100

    Adding State and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, { useState } from "react";
 import "./styles/app.scss";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+//import Util
+import songLibrary from "./util";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(songLibrary());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
