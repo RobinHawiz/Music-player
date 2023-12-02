18fba7bc013d0f647dbed2abe246bca5360f9280
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-02 01:26:32 +0100

    Library UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./styles/app.scss";
 //Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library";
 //import Util
 import songLibrary from "./util";
 
@@ -19,6 +20,7 @@ function App() {
         setIsPlaying={setIsPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
