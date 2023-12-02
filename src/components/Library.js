29c98d933853f1731d6e1329c581eb5c1d6cf031
RobinH <Robin.Hawiz.6670@student.uu.se>
2023-12-02 01:49:01 +0100

    Setting songs

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2 className="library-title">Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            song={song}
+          />
         ))}
       </div>
     </div>
